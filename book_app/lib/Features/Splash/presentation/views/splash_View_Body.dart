afcc300fd1a59a1f87bbe7656c4232e20af991c4
rawd93 <syrawd@gmail.com>
2024-07-26 13:38:21 +0300

    create splash_view_body

---
+++ book_app/lib/Features/Splash/presentation/views/splash_View_Body.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Scaffold(
+body: Text('data'),
+    );
+  }
+}
