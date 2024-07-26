289f8e925375ad905ae4a432a2578e954112d9d9
rawd93 <syrawd@gmail.com>
2024-07-26 18:47:22 +0300

    adding siling animation to splashview

---
+++ book_app/lib/Features/Splash/presentation/views/Widgets/sliding_Animation.dart
@@ -0,0 +1,29 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class SlidingAnimation extends StatelessWidget {
+  const SlidingAnimation({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, child) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: const Text(
+            'Read Free BOOKS',
+            textAlign: TextAlign.center,
+          ),
+        );
+      },
+    );
+  }
+}
--- book_app/lib/Features/Splash/presentation/views/splash_View_Body.dart
+++ book_app/lib/Features/Splash/presentation/views/splash_View_Body.dart
@@ -1,9 +1,40 @@
+import 'package:book_app/Features/Splash/presentation/views/Widgets/sliding_Animation.dart';
 import 'package:book_app/core/Utiles/assets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationcontroller; //بياخد قيمتو ب initState
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationcontroller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation = Tween<Offset>(
+            begin: const Offset(0, 5),
+            end:
+                Offset.zero) //كانت قيم محورية عملتها انيميت ز لان تايب انيميشون
+        .animate(animationcontroller);
+
+    animationcontroller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationcontroller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,12 +49,9 @@ class SplashViewBody extends StatelessWidget {
           ),
         ),
         const SizedBox(
-          height: 5,
+          height: 20,
         ),
-        const Text(
-          'Read Free BOOKS',
-          textAlign: TextAlign.center,
-        ),
+        SlidingAnimation(slidingAnimation: slidingAnimation),
       ],
     );
   }
