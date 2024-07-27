f5d16763a0d2c45f412e5ec311d6a47e8dce3996
rawd93 <syrawd@gmail.com>
2024-07-27 20:29:28 +0300

    navigation to home view& refactor initstate

--- book_app/lib/Features/Splash/presentation/views/splash_View_Body.dart
+++ book_app/lib/Features/Splash/presentation/views/splash_View_Body.dart
@@ -1,8 +1,12 @@
+
+import 'package:book_app/Constatnt.dart';
+import 'package:book_app/Features/Home/data/presentation/views/home_view.dart';
 import 'package:book_app/Features/Splash/presentation/views/Widgets/sliding_Animation.dart';
 import 'package:book_app/core/Utiles/assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -19,15 +23,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationcontroller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    slidingAnimation = Tween<Offset>(
-            begin: const Offset(0, 5),
-            end:
-                Offset.zero) //كانت قيم محورية عملتها انيميت ز لان تايب انيميشون
-        .animate(animationcontroller);
+    initSlidingAnimation();
 
-    animationcontroller.forward();
+    navigationtohomeview();
   }
 
   @override
@@ -54,5 +52,22 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingAnimation(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationcontroller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation = Tween<Offset>(
+            begin: const Offset(0, 5),
+            end:
+                Offset.zero) //كانت قيم محورية عملتها انيميت ز لان تايب انيميشون
+        .animate(animationcontroller);
+    animationcontroller.forward();
+  }
+
+  void navigationtohomeview() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const homeView(), transition: Transition.fadeIn,duration: kTransitionDuration);
+    });
   }
 }
