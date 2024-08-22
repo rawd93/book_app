aaa5541b53862efe5f7f717d98db702f6700704d
rawd93 <syrawd@gmail.com>
2024-08-22 10:52:04 +0300

    create featured book state

---
+++ book_app/lib/Features/Home/data/presentation/manager/featured_books_cubit/featuredbooks_state.dart
@@ -0,0 +1,20 @@
+part of 'featuredbooks_cubit.dart';
+
+@immutable
+sealed class FeaturedbooksState {}
+
+final class FeaturedbooksInitial extends FeaturedbooksState {}
+
+final class FeaturedbooksLoading extends FeaturedbooksState {}
+
+final class FeaturedbooksFailure extends FeaturedbooksState {
+  final String errorMessage;
+
+  FeaturedbooksFailure( this.errorMessage);
+}
+
+final class FeaturedbooksSucsess extends FeaturedbooksState {
+  final List<BookEntity> books;
+
+  FeaturedbooksSucsess(this.books);
+}
